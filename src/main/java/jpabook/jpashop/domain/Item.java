df6475e4c2c4f5c36183e685f22301ab00657fc6
jiokim-7 <67178562+jiokim-7@users.noreply.github.com>
2023-04-04 22:11:11 +0900

    add : Inheritance_Relation_table

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,19 +1,14 @@
 package jpabook.jpashop.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public class Item {
 
     @Id @GeneratedValue
-    @Column(name = "item_id")
     private Long id;
-
     private String name;
     private int price;
-    private int stockQuantity;
 
 }
